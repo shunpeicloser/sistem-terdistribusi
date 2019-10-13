2993a26ab8b25065429eaf70272231fd4727ab9c
tirta <tirtajiwanggataufiq@gmail.com>
2019-10-13 09:43:46 +0700

    update file. masih belum thread

---
+++ tugas-3/pinger.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+class Pinger:
+    id = ""
+    status = True
+    service_list = {}
+    # base_time = datetime(2019, 10, 9)
+
+    def __init__(self, id, services=None):
+        if service_list == None:
+            return "no services exist"
+        
+        self.id = id
+
+        # add services
+        for service in services:
+            if service.id == self.id:
+                continue
+            t = datetime.utcnow()
+            self.service_list[service.id] = [service, t]
+        
+        return "success"
+
+    # send
+    def recv_beat(self, src_id):
+        if self.id == src_id:
+            return
+        
+        while True:
+            try:
+                now = datetime.utcnow()
+                if (now - self.service_list[src_id][1]).total_seconds > 6:
+                    print('Server '+str(dest_id)+' fail. Deleting from service_list')
+                    self.service_list.__delitem__(dest_id)
+                    return
+            except KeyboardInterrupt:
+                return
+
+    # recv
+    @Pyro4.oneway
+    def send_beat(self, dest_id):
+        self.service_list[dest_id][1] = datetime.utcnow()
--- tugas-3/server.py
+++ tugas-3/server.py
@@ -1,4 +1,6 @@
-from filemanager import  *
+from service1 import  *
+from service2 import  *
+from service3 import  *
 import Pyro4
 
 def start_server(host='localhost', port=50001):
@@ -7,10 +9,31 @@ def start_server(host='localhost', port=50001):
     #untuk mengecek service apa yang ada di ns, gunakan pyro4-nsc -n [host] -p [port] list
     daemon = Pyro4.Daemon(host=host)
     ns = Pyro4.locateNS(host, port)
-    expose_server = Pyro4.expose(FileManager)
-    uri = daemon.register(expose_server)
-    print("URI FileManager : ", uri)
-    ns.register("filemanager", uri)
+
+    service1 = Pyro4.expose(Service1)
+    pinger1 = Pyro4.expose(Pinger1)
+
+    service2 = Pyro4.expose(Service2)
+    pinger2 = Pyro4.expose(Pinger2)
+
+    service3 = Pyro4.expose(Service3)
+    pinger3 = Pyro4.expose(Pinger3)
+    
+    uri_s1 = daemon.register(service1)
+    uri_p1 = daemon.register(pinger1)
+    uri_s2 = daemon.register(service2)
+    uri_p2 = daemon.register(pinger2)
+    uri_s3 = daemon.register(service3)
+    uri_p1 = daemon.register(pinger3)
+
+    # print("URI FileManager : ", uri)
+    ns.register("service1", uri_s1)
+    ns.register("pinger1", uri_p1)
+    ns.register("service2", uri_s2)
+    ns.register("pinger2", uri_p2)
+    ns.register("service3", uri_s3)
+    ns.register("pinger3", uri_p3)
+    
     daemon.requestLoop()
 
 if __name__ == '__main__':
